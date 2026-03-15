25ff22b6054c138c9d45b31139c6aa9bd5ec727c
SamuelAdmand <snowden06062013@protonmail.com>
2026-03-15 13:38:13 +0530

    feat: Implement OMR sheet application with PWA support and session persistence.

--- public/sw.js
+++ public/sw.js
@@ -8,13 +8,13 @@ const ASSETS = [
   '/icon-512x512.png'
 ];
 
-self.addEventListener('install', (e: any) => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (e: any) => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
